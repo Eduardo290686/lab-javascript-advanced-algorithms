3ebd1b736a90bc31ef554254e1d90d8124d84fc7
Eduardo <ceciliorubes@gmail.com>
2020-01-11 22:46:19 +0100

    lógica pila terminada

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 7;
+  this.isEmpty = function () {
+    if (this.stackControl.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  this.canPush = function () {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  this.push = function (element) {
+    this.stackControl.push(element);
+    let canPushQuestion = this.canPush();
+    if (canPushQuestion === false) {
+      return 'Stack Overflow';
+    }
+    return this.stackControl;
+  }
+  this.pop = function () {
+    let isEmptyQuestion = this.isEmpty();
+    if (isEmptyQuestion === true) {
+      return 'Stack Underflow';
+    }
+    return this.stackControl[this.stackControl.length-1]
+  }
 }
